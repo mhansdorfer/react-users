f82a801bdba1e8a321beeba1ce27a3273c08b012
Michalina Hansdorfer <hansdorfer.m@gmail.com>
2020-12-13 16:49:46 +0100

    Task 2 - modification added (placeholder)

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,17 +4,21 @@ import UserDetails from './UserDetails.js';
 import '../css/UserList.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import ReactPlaceholder from 'react-placeholder';
+import "react-placeholder/lib/reactPlaceholder.css";
   
 export default class UserList extends React.Component {
     
     state = {
-        users : []
+        users : [], 
+        ready: false
     };
     
     componentDidMount(){
         fetch("https://randomuser.me/api/?results=10")
         .then(response => response.json())
-        .then(data => this.setState({users: data.results}))
+        .then(data => setTimeout(() => this.setState({users: data.results, ready: true}), 2000)) //timeout in order to show placeholder
         .catch(error => console.log(error));
     }
 
@@ -22,14 +26,18 @@ export default class UserList extends React.Component {
         return (
             <Router>
                 <Switch>
-                <Route path="/users/:userId">
-                    <UserDetails users={this.state.users}/>
-                </Route>
-                <Route path="/">
-                    <div className="userList">
-                       {this.state.users.map((profile) => <User key={profile.login.uuid} {...profile}/>)}
-                   </div>
-                </Route>
+                    <Route path="/users/:userId">
+                        <UserDetails users={this.state.users}/>
+                    </Route>
+                    <Route path="/">
+                        <div className="userListPLaceholder">
+                            <ReactPlaceholder showLoadingAnimation ready={this.state.ready} type='media' rows={6}>
+                                <div className="userList">
+                                    {this.state.users.map((profile) => <User key={profile.login.uuid} {...profile}/>)}
+                                </div>
+                            </ReactPlaceholder>
+                        </div>,
+                    </Route>
                 </Switch>
             </Router>
         );
